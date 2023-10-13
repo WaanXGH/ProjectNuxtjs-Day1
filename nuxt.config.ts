873e684276823aea56c10d29d844e959ebb8eb78
WaanXGH <najwan.itsk@gmail.com>
2023-10-13 13:23:58 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,42 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
+
+  tailwindcss:{
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "~/tailwind.config.ts",
+  },
+
+  pwa: {
+    manifest: {
+      name:"najwanExProject",
+      short_name:"najwan",
+      theme_color:"#000000",
+      display: "standalone",
+      orientation:"portrait",
+      scope:"/",
+      start_url: "/",
+      icons:[
+        {
+          src:"images/desain96x96px.png",
+          sizes:"96x96",
+          type: "image/png"
+        },
+        {
+          src: "images/desain128x128px.png",
+          sizes:"128x128",
+          type: "image/png",
+        },
+        {
+          src:"images/desain144x144px.png",
+          sizes: "144x144",
+          type:"image/png"
+        },
+      ]
+    },
+    devOptions: {
+      enabled: true,
+    }
+  }
+})
