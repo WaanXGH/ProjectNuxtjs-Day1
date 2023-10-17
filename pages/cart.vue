325345b85868d35f25a2cc4b4631b08ccfc7d9c3
WaanXGH <najwan.itsk@gmail.com>
2023-10-17 15:03:06 +0700

    update pages

---
+++ pages/cart.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import navbar from '~/components/navbar.vue';
+    import Footer from '../components/footer.vue';
+</script>
+
+<template>
+    <navbar />
+    <h1>Ini Halaman cart</h1>
+
+    <Footer />
+</template>
---
+++ pages/product.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import navbar from '~/components/navbar.vue';
+    import Footer from '../components/footer.vue';
+</script>
+
+<template>
+    <navbar />
+    <h1>Ini Halaman product
+    </h1>
+
+    <Footer />
+</template>
