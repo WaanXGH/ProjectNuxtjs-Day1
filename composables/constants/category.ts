58dc32fb90fadad74faaeaffc95b134dadfa5805
WaanXR <140573251+WaanXGH@users.noreply.github.com>
2023-10-24 08:17:23 +0700

    Day-3

---
+++ composables/constants/category.ts
@@ -0,0 +1,38 @@
+export const category: Category[] = [
+    {
+        id: 1,
+        name: "jam",
+    },
+    {
+        id: 2,
+        name: "Sepatu",
+    },
+    {
+        id: 3,
+        name: "Celana",
+    },
+    {
+        id: 4,
+        name: "Baju",
+    },
+    {
+        id: 5,
+        name: "Baju",
+    },
+    {
+        id: 6,
+        name: "Celanam",
+    },
+    {
+        id: 7,
+        name: "Jam",
+    },
+    {
+        id: 8,
+        name: "Sepatu",
+    },
+]
+export interface Category {
+    id?: number,
+    name?: string,
+}
---
+++ pages/product/[id].vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import products from "~/composables/constants/products";
+import ProductsType from "~/types/product";
+import DetailProduct from "~/components/DetailProduct.vue";
+
+const route = useRoute();
+const id = typeof route.params.id === 'string' ? parseInt(route.params.id) :
+    null;
+const product = ref(products.find((item: ProductsType) => item.id === id));
+</script>
+
+<template>
+    <DetailProduct :product="product" />
+</template >
